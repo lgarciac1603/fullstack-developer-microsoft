c666cb1dc34058c0048bc203cfec5647131b28a0
Fernando García <lfgc851@gmail.com>
2026-06-02 23:45:39 -0600

    Persistencia de contador y caché en sesión y memoria
    Se integra Blazored.SessionStorage para persistir el contador en Counter.razor y se hace asíncrono el método de incremento. Se registran servicios de sesión y caché en Program.cs, incluyendo CacheService. Se añade FetchData.razor para mostrar datos en caché y se agrega el archivo de solución BlazorServerApp.slnx.

--- fullstack-integration/BlazorServerApp/Program.cs
+++ fullstack-integration/BlazorServerApp/Program.cs
@@ -1,18 +1,27 @@
 using BlazorServerApp.Components;
+using Blazored.SessionStorage;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents();
+builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+});
+
+builder.Services.AddSingleton<CacheService>();
+builder.Services.AddMemoryCache();
+
+builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
@@ -21,7 +30,7 @@ app.UseHttpsRedirection();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode();
+app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+app.UseSession();
 
 app.Run();
